090b8596e2952f9408c27864fc3bfd8929541749
adityasharma36 <adityasharma736630@gmail.com>
2025-08-12 14:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.60%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    void mergeTwoSortedArray(vector<int>& nums1, int m, vector<int>& nums2, int n){
         vector<int>arr(nums1.size(),0);
         int i = 0;
         int start = 0;
@@ -30,5 +30,32 @@ public:
         for(int i = 0;i<arr.size();i++){
             nums1[i]= arr[i];
         }
+    }
+
+    void solveByOptimiseSol(vector<int>& nums1, int m, vector<int>& nums2, int n){
+        int idx = m + n -1;
+        int i = m-1;
+        int j = n-1;
+
+        while(i>=0 && j>= 0){
+            if(nums1[i]>= nums2[j]){
+                nums1[idx]= nums1[i];
+                i--;
+                idx--;
+            }
+            else{
+                nums1[idx]= nums2[j];
+                idx--;
+                j--;
+            }
+        }
+        while(j>=0){
+            nums1[idx]= nums2[j];
+                idx--;
+                j--;
+        }
+    }
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        solveByOptimiseSol(nums1,m,nums2,n);
     }
 };
