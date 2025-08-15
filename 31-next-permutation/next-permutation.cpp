b4d218c088e2cba6f0f920effe5b8b429b090ffd
adityasharma36 <adityasharma736630@gmail.com>
2025-08-15 16:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.51%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    void lexicoPermutation(vector<int>& nums){
-        int len = nums.size()-1;
-        int pivot = -1;
-        for(int i = len-1;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                pivot = i;
-                break;
-            }
-          
-        }
-          if(pivot == -1){
-                sort(nums.begin(),nums.end());
-                return;
-            }
-        for(int i = len;i>pivot ;i--){
-            if(nums[i]>nums[pivot]){
-                swap(nums[i],nums[pivot]);
+    void permutations(vector<int>& nums){
+        int pivotIndex = -1;
+        for(int i= nums.size()-2;i>=0;i--){
+            if(nums[i]< nums[i+1]){
+                pivotIndex = i;
                 break;
             }
         }
-        int next = pivot +1;
-        while(next < len){
 
-            swap(nums[next],nums[len]);
-            next++;
-            len--;
+        if(pivotIndex == -1){
+            sort(nums.begin(),nums.end());
+            return;
         }
+        
+
+        for(int i =nums.size()-1;i>pivotIndex;i--){
+            if(nums[pivotIndex]< nums[i]){
+                swap(nums[pivotIndex], nums[i]);
+                break;
+            }
+        }
+        int end = nums.size()-1;
+        while(pivotIndex+1<end){
+            swap(nums[pivotIndex+1],nums[end]);
+            end--;
+            pivotIndex++;
+        }
+
+        
     }
     void nextPermutation(vector<int>& nums) {
-        lexicoPermutation(nums);
+        permutations(nums);
+        
     }
 };
