e0ae92c22d3e4d92b154a79fc83bbac5ee9e3c9d
adityasharma36 <adityasharma736630@gmail.com>
2025-08-15 15:52:42 +0530

    Time: 3 ms (70.60%) | Memory: 14.8 MB (36.06%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    // vector<int> twoSums(vector<int>& nums, int target){
-    //     for(int i = 0;i<nums.size();i++){
-    //         for(int j = i+1;j<nums.size();j++){
-    //             if(nums[i]+ nums[j] == target){
-    //                 return {i,j};
-    //             }
-    //         }
-    //     }
-    //     return {-1,-1};
-    // }
-
-    vector<int> twoSums(vector<int>& nums, int target){
-
-        unordered_map<int,int>um;
-
+    vector<int> sumTwo(vector<int>& nums,int target){
+        // int sum = 0;
+        unordered_map<int,int> um;
         for(int i = 0;i<nums.size();i++){
-            int findVal = target-nums[i];
-
-            if(um.find(findVal) != um.end()){
-                int index = um[findVal];
+            int ele = nums[i];
+            int diff = target- ele;
+            if(um.find(diff) != um.end()){
+                int index = um[diff];
                 return {i,index};
             }
-
-            um[nums[i]] = i;
+            um[ele]= i;
         }
         return {-1,-1};
     }
     vector<int> twoSum(vector<int>& nums, int target) {
-
-        vector<int> answer = twoSums(nums,target);
-        
+        vector<int> answer = sumTwo(nums,target);
         return answer;
-        
     }
 };
