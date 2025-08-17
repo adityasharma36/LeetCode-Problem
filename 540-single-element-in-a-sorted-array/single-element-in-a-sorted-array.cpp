5c8f9a7cdd7c8b39c0f42fc414dbea8b7aa54568
adityasharma36 <adityasharma736630@gmail.com>
2025-08-17 18:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.54%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -15,9 +15,51 @@ public:
         return -1;
     }
 
+    int findSingleBySAS(vector<int>& nums){
 
+        int start = 1;
+
+        int len = nums.size();
+
+        int end = len-2;
+
+        if(len == 1){
+
+            return nums[0];
+
+        }
+        if(nums[0] != nums[start]) return nums[0];
+
+        if(nums[len-1]!= nums[end]) return nums[len-1];
+
+        while(start<=end){
+
+            int mid = start + (end-start)/2;
+
+            if((nums[mid] != nums[mid-1]) && (nums[mid] != nums[mid +1])){
+
+                return nums[mid];
+
+            }
+
+            if((mid %2 == 1 && nums[mid] == nums[mid -1]) || (mid %2 == 0 && nums[mid] == nums[mid +1])){
+
+                start = mid +1;
+
+            } 
+            else{
+
+                end = mid-1;
+
+            }
+        }
+        return -1;
+    }
     int singleNonDuplicate(vector<int>& nums) {
-        int answer = findSingleByMap(nums);
+        
+        // int answer = findSingleByMap(nums);
+
+        int answer = findSingleBySAS(nums);
         return answer;
     }
 };
