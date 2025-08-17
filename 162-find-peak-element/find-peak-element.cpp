7819d9aa4a3cc598a1fbff567dfc96d836ccb218
adityasharma36 <adityasharma736630@gmail.com>
2025-08-17 22:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (67.32%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -18,8 +18,31 @@ public:
         }
         return -1;
     }
+    int findPeakEle(vector<int>& nums){
+        int start = 1;
+        int len = nums.size();
+        int end = len-1;
+        if(len == 1) return 0;
+        if(nums[0]> nums[1]) return 0;
+        if(nums[len-1]> nums[len-2]) return len-1;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if((nums[mid-1]< nums[mid]) && (nums[mid]> nums[mid + 1])) return mid;
+
+            else if(nums[mid-1]< nums[mid]){
+                start = mid + 1;
+            }else{
+                end = mid -1;
+            }
+        }
+        return -1;
+    }
     int findPeakElement(vector<int>& nums) {
-        int answer = bruteForce(nums);
+
+        // int answer = bruteForce(nums);
+
+        int answer = findPeakEle(nums);
         return answer;
     }
 };
