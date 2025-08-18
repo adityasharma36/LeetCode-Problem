094c33e76e7768bdcaaa3dfdb1117212c96b7bc6
adityasharma36 <adityasharma736630@gmail.com>
2025-08-18 20:46:57 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (27.35%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -17,8 +17,26 @@ public:
         }
         return -1;
     }
+    int optimiseSol(vector<int>& arr,int k)
+{
+    int start = 0;
+    int end = arr.size()-1;
+    while(start<=end){
+        int mid = start + (end-start)/2;
+        int missingNum = arr[mid] -(mid +1);
+
+        if(missingNum < k){
+            start = mid + 1;
+        }else{
+            end = mid -1;
+        }
+    }
+    return end + k + 1;
+}
     int findKthPositive(vector<int>& arr, int k) {
-        int answer = kthNumber(arr,k);
+        // int answer = kthNumber(arr,k);
+
+        int answer = optimiseSol(arr,k);
         return answer;
     }
 };
