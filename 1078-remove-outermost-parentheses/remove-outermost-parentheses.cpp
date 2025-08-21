258780842e3526621829effdadbe378bdc0db33b
adityasharma36 <adityasharma736630@gmail.com>
2025-08-21 17:45:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (13.98%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -36,8 +36,30 @@ public:
         }
         return ans;
     }
+    string removeParenthesesByStack(string s){
+        stack<char> st;
+        string ans;
+
+        for(int i = 0;i<s.size();i++){
+            char ch = s[i];
+            if(ch == '('){
+                if(st.size()>0){
+                    ans+=ch;
+                }
+                st.push(ch);
+            }else{
+
+                if(st.size()>1 ){
+                    ans+=ch;
+                }
+                st.pop();
+            }
+        }
+        return ans;
+    }
     string removeOuterParentheses(string s) {
-        string answer = removeParentheses(s);
+        // string answer = removeParentheses(s);
+        string answer = removeParenthesesByStack(s);
         return answer;
     }
 };
