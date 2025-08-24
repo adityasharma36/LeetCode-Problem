515feac5af4c0b50ac9ded35efd23368928d992e
adityasharma36 <adityasharma736630@gmail.com>
2025-08-24 18:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (81.19%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -25,7 +25,41 @@ public:
 
         return ans;
     }
+    void addTwoStringByRec(string& num1,int num1Len,string& num2,int num2Len,int carry,string &ans){
+
+
+        // base case pata nhiii
+        if(num1Len<0 && num2Len <0 && carry == 0){
+           
+            return;
+        }
+
+        // main logic 
+
+        int num1Digit = num1Len >= 0 ? (num1[num1Len]-'0') : 0;
+
+            int num2Digit = num2Len >= 0 ? (num2[num2Len]-'0') : 0;
+
+            int twoDigitSum = num1Digit + num2Digit;
+
+            twoDigitSum+=carry;
+
+            int pushDigit = twoDigitSum% 10;
+
+            ans+=(pushDigit + '0');
+
+            carry = twoDigitSum/10;
+
+            addTwoStringByRec(num1,num1Len-1,num2,num2Len-1,carry,ans);
+
+
+    
+    }
     string addStrings(string num1, string num2) {
-        return addTwoString(num1,num2);
+        // return addTwoString(num1,num2);
+        string ans = "";
+         addTwoStringByRec(num1,num1.size()-1,num2,num2.size()-1,0,ans);
+            reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
