32ac7df36c11a9bd7740cbbf81d5619810b4d060
adityasharma36 <adityasharma736630@gmail.com>
2025-08-29 00:07:06 +0530

    Time: 3 ms (35.27%) | Memory: 10.5 MB (66.75%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
-    void subSet2(vector<int>& nums,vector<vector<int>>& sub,vector<int> & subSet,int index){
-        if(index == nums.size()) {
-            sub.push_back(subSet);
-            return;
-        }
+    // void subSet2(vector<int>& nums,vector<vector<int>>& sub,vector<int> & subSet,int index){
+        // if(index == nums.size()) {
+        //     sub.push_back(subSet);
+        //     return;
+        // }
 
 
-        subSet.push_back(nums[index]);
-        subSet2(nums,sub,subSet,index+1);
-        subSet.pop_back();
+        // subSet.push_back(nums[index]);
+        // subSet2(nums,sub,subSet,index+1);
+        // subSet.pop_back();
 
-        int nextIndex = index + 1;
-        while (nextIndex < nums.size() && nums[nextIndex] == nums[index]) {
-            nextIndex++;
-        }
-        subSet2(nums,sub,subSet,nextIndex);
+    //     int nextIndex = index + 1;
+    //     while (nextIndex < nums.size() && nums[nextIndex] == nums[index]) {
+    //         nextIndex++;
+    //     }
+    //     subSet2(nums,sub,subSet,nextIndex);
         
+    // }
+
+    void subSet2(vector<int>& nums,vector<vector<int>>& sub,vector<int> & subSet,int index){
+
+        //     if(index == nums.size()) {
+        //     sub.push_back(subSet);
+        //     return;
+        // }
+         sub.push_back(subSet);
+
+        for(int i = index;i<nums.size();i++){
+            
+            if(i>index && nums[i] == nums[i-1]) continue;   
+        
+            subSet.push_back(nums[i]);
+            subSet2(nums,sub,subSet,i+1);
+            subSet.pop_back();
+        }
+
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> sub;
