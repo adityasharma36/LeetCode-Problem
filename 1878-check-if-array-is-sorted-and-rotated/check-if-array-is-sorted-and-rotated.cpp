8ac5c4121dc76b331d82aa0438459b54f12afae3
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-08 22:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (44.08%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
+    // bool checking(vector<int>& nums){
+    //     vector<int> newArr;
+    //     int ind = -1;
+    //     for(int i = 1;i<nums.size();i++){
+    //         if(nums[i]< nums[i-1]){
+    //             ind = i;
+    //             break;
+    //         }
+    //     }
+
+    //     cout<<"index "<< ind<<endl;
+
+    //     if(ind == -1) return true;
+
+    //     for(int i = ind;i<nums.size();i++){
+    //         newArr.push_back(nums[i]);
+    //     }
+
+    //     for(auto i :newArr){
+    //         cout<<i<<" ";
+    //     }
+    //     cout<<endl;
+
+    //     for(int i = 0;i<ind;i++){
+    //         newArr.push_back(nums[i]);
+    //     }
+
+    //      for(auto i :newArr){
+    //         cout<<i<<" ";
+    //     }
+
+
+    //     for(int i = 1;i<nums.size();i++){
+    //         if(newArr[i]<newArr[i-1]) return false;
+    //     }
+
+    //     return true;
+    // }   
+
     bool checking(vector<int>& nums){
-        vector<int> newArr;
-        int ind = -1;
-        for(int i = 1;i<nums.size();i++){
-            if(nums[i]< nums[i-1]){
-                ind = i;
-                break;
+        int count = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]> nums[(i+1) % nums.size()]){
+                count++;
             }
+            if(count>1) return false;
         }
-
-        cout<<"index "<< ind<<endl;
-
-        if(ind == -1) return true;
-
-        for(int i = ind;i<nums.size();i++){
-            newArr.push_back(nums[i]);
-        }
-
-        for(auto i :newArr){
-            cout<<i<<" ";
-        }
-        cout<<endl;
-
-        for(int i = 0;i<ind;i++){
-            newArr.push_back(nums[i]);
-        }
-
-         for(auto i :newArr){
-            cout<<i<<" ";
-        }
-
-
-        for(int i = 1;i<nums.size();i++){
-            if(newArr[i]<newArr[i-1]) return false;
-        }
-
         return true;
     }
     bool check(vector<int>& nums) {
