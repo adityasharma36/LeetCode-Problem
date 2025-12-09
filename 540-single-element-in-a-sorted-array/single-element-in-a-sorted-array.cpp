0fd1fb5eea3ddf1a3662bf0840044ae676416acc
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-09 22:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (7.59%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,65 +1,30 @@
 class Solution {
 public:
-
-    int findSingleByMap(vector<int>& nums){
-        unordered_map<int,int> um;
-        for(int i = 0;i<nums.size();i++){
-            um[nums[i]]++;
-        }
-        for(auto & mapping : um){
-        
-            if(mapping.second == 1){
-                return mapping.first;
-            }
-        }
-        return -1;
-    }
-
-    int findSingleBySAS(vector<int>& nums){
-
+    int singleEle(vector<int>& nums){
         int start = 1;
-
         int len = nums.size();
-
         int end = len-2;
 
-        if(len == 1){
+        if(len == 1) return nums[0];
 
-            return nums[0];
-
-        }
-        if(nums[0] != nums[start]) return nums[0];
-
+        if(nums[0]!= nums[start]) return nums[0];
         if(nums[len-1]!= nums[end]) return nums[len-1];
 
         while(start<=end){
+            int mid = (end-start)/2 + start;
 
-            int mid = start + (end-start)/2;
+            if((nums[mid] != nums[mid-1]) && (nums[mid] != nums[mid+1])) return nums[mid];
 
-            if((nums[mid] != nums[mid-1]) && (nums[mid] != nums[mid +1])){
 
-                return nums[mid];
-
-            }
-
-            if((mid %2 == 1 && nums[mid] == nums[mid -1]) || (mid %2 == 0 && nums[mid] == nums[mid +1])){
-
-                start = mid +1;
-
-            } 
-            else{
-
+            if((mid % 2 == 1 && nums[mid] == nums[mid-1] ) ||  (mid %2 == 0 && nums[mid] ==nums[mid+1])){
+                start = mid+1;
+            }else{
                 end = mid-1;
-
             }
         }
         return -1;
     }
     int singleNonDuplicate(vector<int>& nums) {
-        
-        // int answer = findSingleByMap(nums);
-
-        int answer = findSingleBySAS(nums);
-        return answer;
+        return singleEle(nums);
     }
 };
