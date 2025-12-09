17850118c3d79ea95cc95e4bfd1af2e50185ee2b
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-09 22:43:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (10.03%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,48 +1,25 @@
+
 class Solution {
 public:
-    int bruteForce(vector<int>& nums){
-        int len = nums.size()-1;
-
-        if(nums.size() == 1) return 0;
-
-        if(nums[0]> nums[1]) return 0;
-
-        if(nums[len] > nums[len-1]) return len;
-
-        for(int i = 1;i<nums.size()-1;i++){
-
-            if((nums[i]>nums[i+1]) && (nums[i] > nums[i-1])){
-                return i;
-            }
-
-        }
-        return -1;
-    }
-    int findPeakEle(vector<int>& nums){
-        int start = 1;
-        int len = nums.size();
-        int end = len-1;
-        if(len == 1) return 0;
-        if(nums[0]> nums[1]) return 0;
-        if(nums[len-1]> nums[len-2]) return len-1;
-
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if((nums[mid-1]< nums[mid]) && (nums[mid]> nums[mid + 1])) return mid;
-
-            else if(nums[mid-1]< nums[mid]){
-                start = mid + 1;
-            }else{
-                end = mid -1;
-            }
-        }
-        return -1;
-    }
     int findPeakElement(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
 
-        // int answer = bruteForce(nums);
+        while(start < end){
+            int mid = start + (end - start) / 2;
 
-        int answer = findPeakEle(nums);
-        return answer;
+            // if mid element is less than next element: peak is right side
+            if(nums[mid] < nums[mid + 1]){
+                start = mid + 1;
+            } 
+            // else peak is left side (including mid)
+            else {
+                end = mid;
+            }
+        }
+
+        return start; // or end (both same)
     }
 };
+
+
