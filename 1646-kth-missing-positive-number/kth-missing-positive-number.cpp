1cdeb77140d3c498f011ba3c52751502cbc9140f
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-15 22:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (90.28%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-    int kthNumber(vector<int>& arr,int k){
-        int start = 1;
-        int count = 0;
-        int i = 0;
+    int findKth(vector<int>& arr,int k){
+        int start = 0;
+        int end = arr.size()-1;
+        while(start<=end){
+            int mid = (end-start)/2 + start;
 
-        while(true){
-            if(i< arr.size() && arr[i]== start){
-                i++;
-            }
-            else{
-                count++;
-                if(count == k) return start;
-            }
-            start++;
-        }
-        return -1;
-    }
-    int optimiseSol(vector<int>& arr,int k)
-{
-    int start = 0;
-    int end = arr.size()-1;
-    while(start<=end){
-        int mid = start + (end-start)/2;
-        int missingNum = arr[mid] -(mid +1);
+            int findMissingEle = arr[mid]-(mid+1);
 
-        if(missingNum < k){
-            start = mid + 1;
-        }else{
-            end = mid -1;
+            if(findMissingEle<k){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
         }
+        return end + k + 1;
     }
-    return end + k + 1;
-}
     int findKthPositive(vector<int>& arr, int k) {
-        // int answer = kthNumber(arr,k);
-
-        int answer = optimiseSol(arr,k);
-        return answer;
+        return findKth(arr,k);
     }
 };
