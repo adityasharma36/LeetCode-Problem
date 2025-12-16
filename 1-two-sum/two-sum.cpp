d7b7b334a34c38b8e0e323b46a43c2809858b81b
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-16 22:15:14 +0530

    Time: 7 ms (42.51%) | Memory: 18.1 MB (5.97%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,15 +2,61 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        unordered_map<int,int> um;
+        // sabka logic hota hai
+        // unordered_map<int,int> um;
 
+        // for(int i = 0;i<nums.size();i++){
+        //     int ele = nums[i];
+        //     int check = target - ele;
+        //     if(um.find(check) != um.end()){
+        //         return {um[check],i};
+        //     }
+        //     um[ele] = i;
+        // }
+        // return {-1,-1};
+
+        
+
+        //  Method 2 --> Mast Code hai
+
+        unordered_map<int,vector<int>> um;
         for(int i = 0;i<nums.size();i++){
             int ele = nums[i];
-            int check = target - ele;
-            if(um.find(check) != um.end()){
-                return {um[check],i};
+            um[ele].push_back(i);
+        }
+
+        for(int i = 0;i<nums.size();i++){
+            int curEle = nums[i];
+            int required = target-nums[i];
+
+            if(um.find(required) != um.end()){
+
+                // found
+
+                //  1 element ko 1 hi baar use karna hai
+                // duplicate element wale case ko bhi handle karna hai
+
+
+                // duplicate element wala case
+                if(curEle == required && um[curEle].size() == 1){
+
+                    // iska matlab main same element ko 2 baar use kar raha hu
+                    continue;
+
+                }
+
+                if(required == curEle && um[curEle].size() > 1){
+
+                    //  duplicate case 
+                    return {um[curEle][0], um[curEle][1]};
+
+                }
+                else{
+                    
+                    // reuired != current
+                    return {i,um[required][0]};
+                }
             }
-            um[ele] = i;
         }
         return {-1,-1};
     }
