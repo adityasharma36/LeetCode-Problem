eb347f191d943a5d5660754843a9920c3ee2886a
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-16 23:08:30 +0530

    Time: 1 ms (25.07%) | Memory: 9 MB (52.64%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string removeOuterParentheses(string s) {
+    string outerParent(string s){
         int cnt = 0;
         string ans = "";
         for(int i = 0;i<s.size();i++){
@@ -19,6 +19,31 @@ public:
                 }
             }
         }
+        return ans;
+    }
+    string stackMethod(string s){
+        stack<char> st;
+        string ans = "";
+        for(int i = 0;i<s.size();i++){
+            char ch = s[i];
+
+            if(ch == '('){
+                if(!st.empty()){
+                    ans+=ch;
+                }
+                st.push(ch);
+            }else{
+                st.pop();
+                if(!st.empty()){
+                    ans+=ch;
+                }
+            }
+        }
+        return ans;
+    }
+    string removeOuterParentheses(string s) {
+        // string ans = outerParent(s);
+        string ans = stackMethod(s);
         return ans;
     }
 };
