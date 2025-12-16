dc6e79e2dd76f82751d1f255be9b0333642cfd47
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-16 22:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (79.65%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,65 +1,24 @@
 class Solution {
 public:
-    string removeParentheses(string s){
-
-        int balance = 0;
-        
-        string ans;
-        
-        for(int i= 0;i<s.size();i++){
-        
-            char ch = s[i];
-
-            if(ch == '('){
-                
-                if(balance > 0){
-                
-                    ans+=ch;
-                
-                }
-                
-                balance++;
-            }
-            
-            else{
-            
-                balance--;
-            
-                if(balance >0 ){
-            
-                        ans+=ch;
-            
-                    }
-            
-             }
-            
-        }
-        return ans;
-    }
-    string removeParenthesesByStack(string s){
-        stack<char> st;
-        string ans;
-
-        for(int i = 0;i<s.size();i++){
-            char ch = s[i];
-            if(ch == '('){
-                if(st.size()>0){
-                    ans+=ch;
-                }
-                st.push(ch);
-            }else{
-
-                if(st.size()>1 ){
-                    ans+=ch;
-                }
-                st.pop();
-            }
-        }
-        return ans;
-    }
     string removeOuterParentheses(string s) {
-        // string answer = removeParentheses(s);
-        string answer = removeParenthesesByStack(s);
-        return answer;
+        int cnt = 0;
+        string ans = "";
+        for(int i = 0;i<s.size();i++){
+            
+            char ch = s[i];
+
+            if(ch == '('){
+                if(cnt>0){
+                    ans+=ch;
+                }
+                cnt++;
+            }else{
+                cnt--;
+                if(cnt>0){
+                    ans+=ch;
+                }
+            }
+        }
+        return ans;
     }
 };
