aa977a9eca8e6de834a954cbbcc19fa44cb7ce99
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-17 22:40:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (8.30%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
+    bool firstMethod(string s,string t){
         unordered_map<char,char> freq;
         unordered_map<char,char>fr;
 
@@ -16,5 +16,26 @@ public:
 
         }
       return true;
+    }
+    bool secondMethod(string s,string t){
+        int hashMap[256]= {0};
+        bool isVisited[256] = {0};
+
+        for(int i = 0;i<s.size();i++){
+
+            if(hashMap[s[i]] == 0 && isVisited[t[i]] == 0){
+                hashMap[s[i]]= t[i];
+                isVisited[t[i]] = true;
+            }
+        }
+         for(int i = 0;i<s.size();i++){
+                if(char(hashMap[s[i]]) != t[i]) return false;
+            }
+        return true;
+    }
+    bool isIsomorphic(string s, string t) {
+        // return firstMethod(s,t);
+
+        return secondMethod(s,t);
     }
 };
