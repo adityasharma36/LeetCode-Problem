343926e603aed1f334c4ff25a459d778f552aca2
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-17 22:58:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (36.83%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
-    // bool stringRotate(string s, string goal){
-        // if(s.size()!= goal.size()) return false;
-    //     int count = 0;
-    //     string finding ;
-    //     for(int i= 0;i<s.size();i++){
-    //         finding+=s[i];
+    // bool uniqeCheck(string s,string g){
+    //     unordered_map<char,int> freq;
 
-    //         if(goal.find(finding) == string::npos){
-    //             count++;
-    //             finding = s[i];
-    //         }
-
+    //     for(int i = 0;i<s.size();i++){
+    //         freq[s[i]]++;
     //     }
-    //     if(count>1) return false;
+    //     for(int j= 0;j<g.size() ;j++){
+    //         freq[g[j]]--;
+    //     }
 
+    //     return (freq.size()==0);
+    // }
+    // bool findEle(string s,string goal){
+    //      int cnt = 0;
+    //     string temp ;
+        
+    //     for(int i = 0;i<s.size();i++){
+    //         temp+=s[i];
+
+    //         if(goal.find(temp)==string::npos){
+    //             cnt++;
+    //             temp = s[i];
+    //         }
+    //     }
+
+    //     if(cnt>1) return false;
     //     return true;
     // }
-
-    bool stringRotate(string s,string goal){
-        if(s.size()!= goal.size()) return false;
-
-        string newString = s+s;
-
-        if(newString.find(goal) != string::npos){
-            return true;
-        }
-        return false;
-    }
+  
     bool rotateString(string s, string goal) {
-        bool answer = stringRotate(s,goal);
-        return answer;
+        
+        if(s.size() != goal.size()) return false;
+
+        string newString = s+ s;
+
+        if(newString.find(goal) == string::npos) return false;
+
+        return true;
     }
 };
