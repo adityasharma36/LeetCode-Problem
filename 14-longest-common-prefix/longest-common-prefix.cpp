a86ee7e6d5e90c00f914b1d57b8cea1cd817489e
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-18 21:59:03 +0530

    Time: 24 ms (5.18%) | Memory: 21 MB (5.10%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,54 +1,61 @@
+class Node{
+    public:
+        char val;
+        unordered_map<char,Node*> um;
+        bool isTerminal;
+
+        Node(char data){
+            this->val = data;
+            this->isTerminal = false;
+        }
+};
+void valueInsert(Node* head,string s){
+    // base case;
+
+    if(s.length()==0){
+        head->isTerminal = true;
+        return;
+    }
+
+    // ek case me solve karunga
+    char firstEle = s[0];
+    Node* child;
+
+    if(head->um.find(firstEle) != head->um.end()){
+        child = head->um[firstEle];
+    }else{
+        child = new Node(firstEle);
+        head->um[firstEle]= child;
+    }
+
+    valueInsert(child,s.substr(1));
+}
+void findLCP(Node* head,string & ans){
+    if(head->isTerminal == true){
+        return;
+    }
+
+    Node* child;
+    if(head->um.size() == 1){
+        for(auto i: head->um){
+            char fir = i.first;
+            ans.push_back(fir);
+            child = i.second;
+        }
+    }else{
+        return;
+    }
+    findLCP(child,ans);
+}
 class Solution {
 public:
-
-    // wrong approach 
-
-    string longCommonPref(vector<string>& strs){
-        string answer = "";
-        if(strs.size()==1) return strs[0];
-        string temp = strs[0];
-
-        for(int i = 1;i<strs.size();i++){
-            string ans = strs[i];
-            string chara ;
-            for(auto an: ans){
-                chara+=an;
-                if(temp.find(chara)!= string::npos){
-                    answer = chara;
-                }
-            }
-            temp = answer;
-        }
-        return answer;
-    }
-    
-
-    // prefix chaiye  
-    // ek string ko fix karke baki ko iterate karege
-    // and prefix string and itereate stirng k start common part find karege
-    // and uske badd prefix stirng me uska substr find kara lenge
-    
-    string longestCommonPref(vector<string>& strs){
-    
-        string ans = strs[0];
-
-        for(int i= 1;i<strs.size();i++){
-
-            string secondStr = strs[i];
-
-            int j = 0;
-            while(j<ans.size() && j<secondStr.size() && ans[j]== secondStr[j]){
-                j++;
-            }
-            ans = ans.substr(0,j);
-
-            if(ans.empty()) return "";
-        }
-        return ans;
-    }
     string longestCommonPrefix(vector<string>& strs) {
-        // return  longCommonPref(strs);
-        return longestCommonPref(strs);
-
+           Node* head = new Node('-');
+        string ans = "";
+        for(auto i: strs){
+            valueInsert(head,i);
+        }
+        findLCP(head,ans);
+        return ans;
     }
 };
