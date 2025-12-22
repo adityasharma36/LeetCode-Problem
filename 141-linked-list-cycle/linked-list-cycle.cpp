aaf168016c535565f8045e261c43f8b90f92e52b
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-22 22:02:50 +0530

    Time: 8 ms (80.15%) | Memory: 11.8 MB (51.48%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
+    bool extraSpace(ListNode* head){
         unordered_map<ListNode*, bool> visited;
         ListNode* temp = head;
 
         while (temp) {
             if (visited[temp]) {
-                return true;   // same node visited again
+                return true;  
             }
             visited[temp] = true;
             temp = temp->next;
         }
         return false;
+    }
+    bool optimiseSol(ListNode* head){
+
+        if(!head) return false;
+        ListNode* toto = head;
+        ListNode* rabi = head;
+
+        while(rabi && rabi->next){
+
+            toto = toto->next;
+            rabi = rabi->next->next;
+
+            if(toto == rabi) return true;
+        
+        }
+        return false;
+    }
+    bool hasCycle(ListNode *head) {
+    //   return extraSpace(head);
+        return optimiseSol(head);
     }
 };
