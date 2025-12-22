6b72e0fcb9767a337cfc7d348ca7d0f91444f302
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-22 22:27:05 +0530

    Time: 10 ms (46.54%) | Memory: 11.3 MB (51.39%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -22,8 +22,31 @@ public:
         }
         return NULL;
     }
+    ListNode* optimiseSpace(ListNode* head){
+
+        ListNode* rabi = head;
+        ListNode* toto = head;
+
+        while(rabi && rabi->next){
+            toto = toto->next;
+            rabi = rabi->next->next;
+
+            if(toto == rabi){
+                toto = head;
+
+                while(toto != rabi){
+                    toto = toto->next;
+                    rabi = rabi->next;
+                }
+                return toto;
+            }
+        }
+        return NULL;
+    }
     ListNode *detectCycle(ListNode *head) {
         
-        return unorMapSol(head);
+        // return unorMapSol(head);
+
+        return optimiseSpace(head);
     }
 };
