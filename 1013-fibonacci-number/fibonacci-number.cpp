5200361a9f5d37554b793ea0d3a748cd8f7e0222
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-24 22:58:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (11.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -7,7 +7,19 @@ public:
         int ans = recursiveOp(n-1)+ recursiveOp(n-2);
         return ans;
     }
+
+    int memoOp(int n,vector<int>&dp){
+        if(n== 0) return 0;
+        if(n==1) return 1;
+        if(dp[n] != -1)return dp[n];
+
+        int ans = memoOp(n-1,dp)+ memoOp(n-2,dp);
+        dp[n]= ans;
+        return dp[n];
+    }
     int fib(int n) {
-        return recursiveOp(n);
+        // return recursiveOp(n);
+        vector<int>dp(n+1,-1);
+        return memoOp(n,dp);
     }
 };
