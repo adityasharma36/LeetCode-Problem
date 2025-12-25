bd281866459f0cd43ef37e87971de1d39a045bfc
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-25 22:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (24.63%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,48 +1,35 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    int getLen(ListNode* head){
-        int count = 0;
-        ListNode * temp = head;
-        while(temp!= NULL){
-            count++;
-            temp = temp->next;
-        }
-        return count;
-    }
-    ListNode* removeNode(ListNode* head,int n){
-        
-        int len = getLen(head);
-        int getEleIndex = len-n;
-        if(n == len) {
-        ListNode* newHead = head->next;
-        delete head;
-        return newHead;
-        }
-        int count = 0;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (!head) return NULL;
+
+        int cnt = 0;
         ListNode* temp = head;
-        while(count<getEleIndex-1){
-            count++;
+
+        // count length
+        while (temp) {
+            cnt++;
             temp = temp->next;
         }
-        ListNode* removeNode = temp->next;
-        ListNode* nextNode = removeNode->next;
-        temp->next = nextNode;
-        removeNode->next = NULL;
-        delete removeNode;
+
+        // if head needs to be removed
+        if (n == cnt) {
+            return head->next;
+        }
+
+        temp = head;
+        int steps = cnt - n;
+
+        // move to node just before target
+        while (steps > 1) {
+            temp = temp->next;
+            steps--;
+        }
+
+        ListNode* curr = temp->next;
+        temp->next = curr->next;
+        curr->next = NULL;
 
         return head;
-    }
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        return removeNode(head,n);
     }
 };
