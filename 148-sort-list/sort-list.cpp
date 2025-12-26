ef87994909fe935824a9015596e68bbe950c5eab
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-26 22:11:16 +0530

    Time: 14 ms (67.50%) | Memory: 57 MB (82.23%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,35 +1,44 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* sortLinkedList(ListNode* head){
-        vector<int> ans;
-        ListNode* temp = head;
-        while(temp != NULL){
-            int val =temp->val;
-            ans.push_back(val);
-            temp = temp->next;
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
         }
-        sort(ans.begin(),ans.end());
-        temp = head;
-        int index = 0;
-        while(temp != NULL && index<ans.size()){
-           temp->val = ans[index];
-           index++;
-            temp = temp->next;
-        }
-        return head;
+
+        if (l1) tail->next = l1;
+        if (l2) tail->next = l2;
+
+        return dummy.next;
     }
+
     ListNode* sortList(ListNode* head) {
-        ListNode* temp = head;
-     return sortLinkedList(temp);
+        if (!head || !head->next) return head;
+
+        // find middle
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* mid = slow->next;
+        slow->next = NULL;
+
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+
+        return merge(left, right);
     }
 };
