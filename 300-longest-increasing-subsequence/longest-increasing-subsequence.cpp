c31daebeb3a539b8f2c08bba1a9f333acc6c409c
Aditya Sharma <adityasharma736630@gmail.com>
2025-12-26 23:31:28 +0530

    Time: 580 ms (9.05%) | Memory: 297 MB (5.87%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // int recursiveSol(vector<int>& nums,int lastIndex,int curr){
+    //     if(curr>=nums.size()) return 0;
+
+    //     int curEle = nums[curr];
+    //     int inc = 0;
+    //     if(lastIndex == -1 || curEle > nums[lastIndex]){
+    //         inc = 1 + recursiveSol(nums,curr,curr+1);
+    //     }
+    //     int exc = recursiveSol(nums,lastIndex , curr+1);
+
+    //     int finalAns = max(inc,exc);
+    //     return finalAns;
+    // }
+    int solveByMemo(vector<int>& nums,vector<vector<int>>& dp,int lastIndex,int curr){
+        if(curr>= nums.size()) return 0;
+
+        if(dp[lastIndex+1][curr]!= -1) return dp[lastIndex+1][curr];
+
+        int incld = 0;
+        int curEle = nums[curr];
+        if(lastIndex==-1 || curEle > nums[lastIndex]){
+            incld = 1+ solveByMemo(nums,dp,curr,curr+1);
+        }
+        int excld = solveByMemo(nums,dp,lastIndex,curr+1);
+
+        dp[lastIndex+1][curr]= max(incld,excld);
+        return dp[lastIndex+1][curr];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        // return recursiveSol(nums,-1,0);
+        vector<vector<int>>dp(nums.size()+2,vector<int>(nums.size()+2,-1));
+        return solveByMemo(nums,dp,-1,0);
+    }
+};
