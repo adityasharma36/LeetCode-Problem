d4902815660cd0c5a9cf50dee824e1711e11d044
Aditya Sharma <adityasharma736630@gmail.com>
2026-01-02 23:00:38 +0530

    Time: 95 ms (69.83%) | Memory: 128.3 MB (12.03%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    bool partitionEqualSubSet(vector<int>& nums,int target,int index){
-        if(target<0 || index == nums.size()) return false;
-        if(target == 0) return true;
-
-        int incld = partitionEqualSubSet(nums,target-nums[index],index+1);
-        int excld = partitionEqualSubSet(nums,target,index+1);
-
-        return incld || excld;
-    }
-
-    bool partitionEqualSubSetByMemo(vector<int>& nums,int target,int index,vector<vector<int>>& ans){
-         if(target<0 || index == nums.size()) return false;
-        if(target == 0) return true;
-
-        if(ans[index][target] != -1) return ans[index][target];
-        int incld = partitionEqualSubSetByMemo(nums,target-nums[index],index+1,ans);
-        int excld = partitionEqualSubSetByMemo(nums,target,index+1,ans);
-
-        ans[index][target] = incld || excld;
-        return ans[index][target];
-    }
-    bool canPartition(vector<int>& nums) {
-
-        int sums  = accumulate(nums.begin(),nums.end(),0);
-        int  target = sums /2;
-
-        if(sums %2 != 0){
-            return false;
+    bool partitionSum(vector<int>& nums,int sum,int start){
+        if(sum == 0){
+            return true;
         }
-        vector<vector<int>>ans(nums.size()+1 , vector<int>(target+1,-1));
-        return partitionEqualSubSetByMemo(nums,target,0,ans);
+        if (start >= nums.size() || sum < 0) return false;
+        
+        return (partitionSum(nums,sum-nums[start],start+1) || partitionSum(nums,sum,start+1));
+     
+      
+    }
+    bool memo(vector<int>& nums, int sum, int start, vector<vector<int>>& dp) {
+        if (sum == 0) return true;
+        if (start >= nums.size() || sum < 0) return false;
+
+        if (dp[sum][start] != -1)
+            return dp[sum][start];
+
+        dp[sum][start] =
+            memo(nums, sum - nums[start], start + 1, dp) ||
+            memo(nums, sum, start + 1, dp);
+
+        return dp[sum][start];
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (total % 2 != 0) return false;
+
+        int target = total / 2;
+        vector<vector<int>> dp(target + 1, vector<int>(nums.size(), -1));
+
+        return memo(nums, target, 0, dp);
     }
 };
