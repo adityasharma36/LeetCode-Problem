968966a12246207260b4cca89185d1fe5b58a089
Aditya Sharma <adityasharma736630@gmail.com>
2026-01-12 22:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (20.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,51 +1,26 @@
 class Solution {
 public:
-    int recursivePro(vector<int>& nums,int i){
-        if(i>= nums.size()) return 0;
+    int robber(vector<int>& nums,int index){
+        if(index>=nums.size()) return 0;
 
-        int include = nums[i]+ recursivePro(nums,i+2);
-        int exclude = recursivePro(nums,i+1);
-        return max(include,exclude);
+        int inclde = nums[index] + robber(nums,index+2);
+        int exlucde = 0 + robber(nums,index+1);
+        return max(inclde,exlucde);
     }
-    int solveByMemo(vector<int>& nums,vector<int>& dp,int i){
-        if(i>= nums.size()) return 0;
-        if(dp[i] != -1) return dp[i];
+    int memo(vector<int>& nums, int index, vector<int>& dp) {
+        if(index >= nums.size()) return 0;
 
-        int include = nums[i] + solveByMemo(nums,dp,i+2);
-        int exclude = solveByMemo(nums,dp,i+1);
-        dp[i]= max(include,exclude);
-        return dp[i];
-    }
-    int solveByTab(vector<int>& nums){
-        vector<int> dp(nums.size()+20 ,0);
+        if(dp[index] != -1) return dp[index];
 
-        for(int i = nums.size()-1;i>= 0;i--){
-        int include = nums[i] + dp[i+2];
-        int exclude = 0+ dp[i+1];
-        dp[i]= max(include,exclude);
-        }
-        return dp[0];
+        int include = nums[index] + memo(nums, index + 2, dp);
+        int exclude = memo(nums, index + 1, dp);
+
+        dp[index] = max(include, exclude);
+        return dp[index];
     }
 
-    int spaceOpt(vector<int>& nums){
-        int next1 = 0;
-        int next2 = 0;
-
-        for(int i = nums.size()-1;i>=0;i--){
-            int include = nums[i] + next2;
-            int exclude = next1;
-            int curr = max(include,exclude);
-            next2 = next1;
-            next1 = curr;
-            
-        }
-        return next1;
-    }
     int rob(vector<int>& nums) {
-        // return recursivePro(nums,0);
-        vector<int>dp(nums.size()+1 ,-1);
-        // return solveByMemo(nums,dp,0);
-        // return solveByTab(nums);
-        return spaceOpt(nums);
+        vector<int> dp(nums.size(), -1);
+        return memo(nums, 0, dp);
     }
 };
