fb56f1199483906fc94ea7834a673a5ef5025d71
Aditya Sharma <adityasharma736630@gmail.com>
2026-01-12 22:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (43.84%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -18,9 +18,24 @@ public:
         dp[index] = max(include, exclude);
         return dp[index];
     }
+    int tabu(vector<int>& nums){
 
+        vector<int>dp(nums.size()+2,0);
+
+        for(int i = nums.size()-1;i>=0;i--){
+
+        int include = nums[i] + dp[i + 2];
+
+        int exclude = dp[i + 1];
+
+        dp[i] = max(include, exclude);
+
+        }
+        return dp[0];
+    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return memo(nums, 0, dp);
+        // vector<int> dp(nums.size(), -1);
+        // return memo(nums, 0, dp);
+        return tabu(nums);
     }
 };
