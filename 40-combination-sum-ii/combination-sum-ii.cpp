e2e9d2485148cbdba8edae82ed6292c099e3b009
Aditya Sharma <adityasharma736630@gmail.com>
2026-01-15 21:47:17 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (38.86%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,50 +1,61 @@
 class Solution {
 public:
-    void combinationSum(vector<int>& candidates, int target,int index ,vector<int>&ans,set<vector<int>> & check){
+    void sumCombination(vector<int>& cand, int target, set<vector<int>>& st,
+                        int start, int end, vector<int>& temp) {
+
         if(target == 0){
-            check.insert(ans);
+
+            st.insert(temp);
+
             return;
         }
-          if (target < 0 || index >= candidates.size()) return;
 
-        ans.push_back(candidates[index]);
-        combinationSum(candidates,target-candidates[index],index+1,ans,check);
-        ans.pop_back();
-        combinationSum(candidates,target,index+1,ans,check);
+        for(int i = start; i < end; i++){
 
-    }
+            if(cand[i] <= target){
 
-    void comboSum2(vector<int>& candidates,int target,int index,vector<vector<int>>& ans,vector<int>& answer){
-        if(target == 0){
-            ans.push_back(answer);
-            return;
-        }
-            if(target<0) return;
+                temp.push_back(cand[i]);
 
+                sumCombination(cand, target - cand[i], st, i + 1, end, temp);
 
-        for(int i = index;i<candidates.size();i++){
-
-            if( i > index && candidates[i] == candidates[i-1] ) continue;
-
-            answer.push_back(candidates[i]);
-
-            comboSum2(candidates,target-candidates[i],i+1,ans,answer);
-
-            answer.pop_back();
+                temp.pop_back();
+            }
         }
     }
+    void memo(vector<int>& cand, int target, int start, int end, vector<int>& temp,vector<vector<int>>&ans){
+            if(target == 0){
+                ans.push_back(temp);
+                return;
+            }
+            for(int i = start;i<end;i++){
+
+                    if(i>start && cand[i]== cand[i-1]) continue;
+
+                    if(cand[i]>target) break;
+
+                    temp.push_back(cand[i]);
+                    memo(cand,target-cand[i],i+1,end,temp,ans);
+                    temp.pop_back();
+            }
+
+        }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-    
-        // vector<int>ans;
-        // set<vector<int>> check;
-        // combinationSum(candidates,target,0,ans,check);
-        //   vector<vector<int>>answer(check.begin(),check.end());
-        //   return answer;
 
-        sort(candidates.begin(),candidates.end());
-        vector<int>answer;
+        sort(candidates.begin(), candidates.end()); // ✅ important
+
+        set<vector<int>> st;
+
+        vector<int> temp;
+
+        int n = candidates.size();
+
+
+        // sumCombination(candidates, target, st, 0, candidates.size(), temp);
+
         vector<vector<int>>ans;
-        comboSum2(candidates,target,0,ans,answer);
+
+        memo(candidates,target,0,n,temp,ans);
+
         return ans;
     }
 };
