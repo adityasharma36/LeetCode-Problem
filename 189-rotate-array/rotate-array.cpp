b25092c1d2a8679ce48806554bbf5c421e9d0bf5
Aditya Sharma <adityasharma736630@gmail.com>
2026-02-16 16:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (91.31%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void swapArray( int start ,int end ,vector<int>& nums){
-        while(start<end){
+    void swappingFnc(vector<int>& nums,int start,int end){
+        while(start<=end){
             swap(nums[start],nums[end]);
             start++;
             end--;
         }
     }
-    void rotateArray(vector<int>& nums,int k){
-        int start =  0;
-        int end = nums.size()-1;
-        swapArray(start,end , nums);
+    void rotationFnc(vector<int>& nums,int k){
+        int swapingIndex = k% nums.size();
 
-        int kRotate = k % nums.size();
-
-        swapArray(start,kRotate-1,nums);
-
-        swapArray(kRotate,end,nums);
+        swappingFnc(nums,0,nums.size()-1);
+        swappingFnc(nums,0,swapingIndex-1);
+        swappingFnc(nums,swapingIndex,nums.size()-1);
     }
     void rotate(vector<int>& nums, int k) {
-        rotateArray(nums,k);
+        rotationFnc(nums,k);
     }
 };
