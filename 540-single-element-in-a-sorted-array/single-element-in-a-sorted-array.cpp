2bcf2bead0e1c4642732d1fa7edc6a72242793e6
Aditya Sharma <adityasharma736630@gmail.com>
2026-03-18 12:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (68.42%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int singleEle(vector<int>& nums){
-        int start = 1;
-        int len = nums.size();
-        int end = len-2;
+    int singleNonDuplicate(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
 
-        if(len == 1) return nums[0];
+        while (start < end) {
+            int mid = start + (end - start) / 2;
 
-        if(nums[0]!= nums[start]) return nums[0];
-        if(nums[len-1]!= nums[end]) return nums[len-1];
+            // Make mid even
+            if (mid % 2 == 1) mid--;
 
-        while(start<=end){
-            int mid = (end-start)/2 + start;
-
-            if((nums[mid] != nums[mid-1]) && (nums[mid] != nums[mid+1])) return nums[mid];
-
-
-            if((mid % 2 == 1 && nums[mid] == nums[mid-1] ) ||  (mid %2 == 0 && nums[mid] ==nums[mid+1])){
-                start = mid+1;
-            }else{
-                end = mid-1;
+            if (nums[mid] == nums[mid + 1]) {
+                start = mid + 2;
+            } else {
+                end = mid;
             }
         }
-        return -1;
-    }
-    int singleNonDuplicate(vector<int>& nums) {
-        return singleEle(nums);
+
+        return nums[start];
     }
 };
