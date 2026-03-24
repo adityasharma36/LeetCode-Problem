04eba74aa171567a130df383709ad94c2eeec107
Aditya Sharma <adityasharma736630@gmail.com>
2026-03-24 18:27:51 +0530

    Time: 4 ms (22.71%) | Memory: 9.5 MB (27.48%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
-    bool firstMethod(string s,string t){
-        unordered_map<char,char> freq;
-        unordered_map<char,char>fr;
-
-        for(int i = 0;i< s.size();i++){
-            char st = s[i];
-            char ct = t[i];
-
-            if(freq.count(st) && freq[st] != ct) return false;
-            if(fr.count(ct) && fr[ct] != st) return false;
-
-            freq[st] = ct;
-            fr[ct] = st;
-
-        }
-      return true;
-    }
-    bool secondMethod(string s,string t){
-        int hashMap[256]= {0};
-        bool isVisited[256] = {0};
-
-        for(int i = 0;i<s.size();i++){
-
-            if(hashMap[s[i]] == 0 && isVisited[t[i]] == 0){
-                hashMap[s[i]]= t[i];
-                isVisited[t[i]] = true;
-            }
-        }
-         for(int i = 0;i<s.size();i++){
-                if(char(hashMap[s[i]]) != t[i]) return false;
-            }
-        return true;
-    }
     bool isIsomorphic(string s, string t) {
-        // return firstMethod(s,t);
+        if(s.size() != t.size()) return false;
 
-        return secondMethod(s,t);
+        unordered_map<char,char> m1;
+        unordered_map<char,char> m2;
+
+        for(int i = 0; i < s.size(); i++){
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if(m1.count(c1) && m1[c1] != c2) return false;
+            if(m2.count(c2) && m2[c2] != c1) return false;
+
+            m1[c1] = c2;
+            m2[c2] = c1;
+        }
+
+        return true;
     }
 };
