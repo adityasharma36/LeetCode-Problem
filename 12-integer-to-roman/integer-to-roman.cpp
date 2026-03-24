1bfaf39030067bd238ac311a07194382e9fa0d35
Aditya Sharma <adityasharma736630@gmail.com>
2026-03-24 21:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (80.42%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    string intergerToRoman(int num){
-        string roman[]= {"M", "CM", "D", "CD","C","XC","L","XL","X","IX","V","IV","I"};
+    string intToRom(int num){
+       string roman[]= {"M", "CM", "D", "CD","C","XC","L","XL","X","IX","V","IV","I"};
         int interger[]= {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-
-        string answer;
-
+        string romanStri;
         for(int i = 0;i<13;i++){
-
             while(num>=interger[i]){
-                
+                romanStri+=roman[i];
                 num-=interger[i];
-                answer+=roman[i];
             }
         }
-        return answer;
+        return romanStri;
     }
     string intToRoman(int num) {
-        string answer = intergerToRoman(num);
-        return answer;
+        return intToRom(num);
     }
 };
