94a6f61dfa3d979c8b41dc91286624bca1fa4bd3
Aditya Sharma <adityasharma736630@gmail.com>
2026-03-24 18:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (85.02%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
-    int countMethod(string s){
-         int cnt = 0;
+    int depthMax(string s){
+        int cnt = 0;
         int temp = 0;
-        for(int i= 0;i<s.size();i++){
-            if(s[i] == '('){
+        for(auto i: s){
+            if(i =='('){
                 temp++;
-            }else if(s[i]== ')'){
                 cnt = max(cnt,temp);
+            }else if(i == ')'){
                 temp--;
             }
         }
-        cnt = max(cnt, temp);
+        cnt = max(cnt,temp);
         return cnt;
     }
-    int stackMethod(string s){
-    stack<char> st;
-    int cnt = 0;
-
-    for(int i = 0; i < s.size(); i++){
-        if(s[i] == '('){
-            st.push(s[i]);
-            cnt = max(cnt, (int)st.size());
-        }
-        else if(s[i] == ')'){
-            if(!st.empty()){
-                st.pop();
-            }
-        }
-    }
-    return cnt;
-}
-
     int maxDepth(string s) {
-    //    return countMethod(s);
-    return stackMethod(s);
+        return depthMax(s);
     }
 };
