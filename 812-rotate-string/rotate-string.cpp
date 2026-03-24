f9038c2e0ea5eba30438e6e4209fdbb184a00b7a
Aditya Sharma <adityasharma736630@gmail.com>
2026-03-24 17:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (24.08%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,42 +1,12 @@
 class Solution {
 public:
-    // bool uniqeCheck(string s,string g){
-    //     unordered_map<char,int> freq;
-
-    //     for(int i = 0;i<s.size();i++){
-    //         freq[s[i]]++;
-    //     }
-    //     for(int j= 0;j<g.size() ;j++){
-    //         freq[g[j]]--;
-    //     }
-
-    //     return (freq.size()==0);
-    // }
-    // bool findEle(string s,string goal){
-    //      int cnt = 0;
-    //     string temp ;
-        
-    //     for(int i = 0;i<s.size();i++){
-    //         temp+=s[i];
-
-    //         if(goal.find(temp)==string::npos){
-    //             cnt++;
-    //             temp = s[i];
-    //         }
-    //     }
-
-    //     if(cnt>1) return false;
-    //     return true;
-    // }
-  
+    bool stringRotate(string s, string g){
+        if(s.size()!= g.size()) return false;
+        s = s+s;
+        cout<<s;
+        return s.find(g) != string::npos;
+    }
     bool rotateString(string s, string goal) {
-        
-        if(s.size() != goal.size()) return false;
-
-        string newString = s+ s;
-
-        if(newString.find(goal) == string::npos) return false;
-
-        return true;
+        return stringRotate(s,goal);
     }
 };
