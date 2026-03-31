e01c23bde3fdf02123a52d3794480346b9cadf50
Aditya Sharma <adityasharma736630@gmail.com>
2026-03-31 18:21:37 +0530

    Time: 72 ms (7.55%) | Memory: 9.2 MB (45.36%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    // int firstOccurence(string haystack, string needle){
-    //     int j = 0;
-    //     int i = 0;
-    //     while(i<haystack.size()){
-    //         if(haystack[i]== needle[j]){
-    //             i++;
-    //             j++;
-    //         }
-    //         else{
-    //             i++;
-    //             j = 0;
-    //         }
-    //         if(j>=needle.size()) return i - needle.size();
-    //     }
-    //     return -1;
-    // }
-    int firstOccurence(string haystack,string needle){
+    int strSub(string haystack, string needle) {
 
-    
-        int haystackLen = haystack.size();
-        int needleLen = needle.size();
-        for(int i = 0;i<=haystackLen-needleLen;i++){
-            for(int j = 0;j<needleLen;j++){
-                if(needle[j] != haystack[i+j]){
-                    break;
+        if (haystack.length() < needle.length())
+            return -1;
+
+        for (int i = 0; i < haystack.length(); i++) {
+            char ch = haystack[i];
+            if (ch == needle[0]) {
+                int temp = i;
+                int j = 0;
+                while (j < needle.size() && temp < haystack.length() &&
+                       haystack[temp] == needle[j]) {
+                        if (j + 1 == needle.length())
+                        return i;
+                        
+                    temp++;
+                    j++;
+                    
                 }
-                if(j == needleLen-1) return i;
             }
         }
         return -1;
     }
     int strStr(string haystack, string needle) {
-        int answer = firstOccurence(haystack,needle);
-        return answer;
+        return strSub(haystack, needle);
     }
 };
