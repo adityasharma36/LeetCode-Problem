582ceddf37f62f5c6ec3d698c488e419ef4d817e
Aditya Sharma <adityasharma736630@gmail.com>
2026-04-22 18:47:09 +0530

    Time: 5 ms (22.00%) | Memory: 11.5 MB (22.69%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,57 +1,57 @@
 class Solution {
 public:
-    unordered_map<int,bool>rowCheck;
-    unordered_map<int,bool> upperDiagonalCheck;
+    unordered_map<int,bool> rowCheck;
     unordered_map<int,bool> lowerDiagonalCheck;
-    void storeValue(vector<vector<string>>& ans,vector<vector<char>>& board,int n){
-        vector<string> check;
-        for(int i = 0;i<n;i++){
-            string output = "";
-            for(int j = 0;j<n;j++){
-                output.push_back(board[i][j]);
-            }
-            check.push_back(output);
-        }
-        ans.push_back(check);
-    }
-    bool isPossible(vector<vector<char>>board,int col,int row){
-        if(rowCheck[row]== true){
-            return false;
-        }
-        if(lowerDiagonalCheck[col+row]==true){
-            return false;
-        }
-        if(upperDiagonalCheck[row-col]==true){
-            return false;
-        }
+    unordered_map<int,bool> upperDiagonalCheck;
+
+    bool isPossible(int row, int col) {
+        if(rowCheck[row]) return false;
+        if(lowerDiagonalCheck[row + col]) return false;
+        if(upperDiagonalCheck[row - col]) return false;
         return true;
     }
-    void solve(vector<vector<string>>& ans,vector<vector<char>>& board,int col,int n){
-        if(col>=n){
-            // all queen is palced we have to arrage it
-            // store in ans array
-            storeValue(ans,board,n);
+
+    void storeAnswer(vector<vector<string>>& ans, vector<vector<char>>& board, int n) {
+        vector<string> temp;
+        for(int i = 0; i < n; i++) {
+            string row = "";
+            for(int j = 0; j < n; j++) {
+                row += board[i][j];
+            }
+            temp.push_back(row);
+        }
+        ans.push_back(temp);
+    }
+
+    void solve(int n, vector<vector<string>>& ans, vector<vector<char>>& board, int col) {
+        if(col >= n) {
+            storeAnswer(ans, board, n);
             return;
         }
-         for(int row = 0;row<n;row++){
-            if(isPossible(board , col , row)){
-                board[row][col]= 'Q';
+
+        for(int row = 0; row < n; row++) {
+            if(isPossible(row, col)) {
+
+                board[row][col] = 'Q';
                 rowCheck[row] = true;
-                upperDiagonalCheck[row-col] = true;
-                lowerDiagonalCheck[row+col]= true;
-                solve(ans,board,col+1,n);
-                board[row][col]= '.';
+                lowerDiagonalCheck[row + col] = true;
+                upperDiagonalCheck[row - col] = true;
+
+                solve(n, ans, board, col + 1);
+
+                board[row][col] = '.';
                 rowCheck[row] = false;
-                upperDiagonalCheck[row-col] = false;
-                lowerDiagonalCheck[row+col]= false;
+                lowerDiagonalCheck[row + col] = false;
+                upperDiagonalCheck[row - col] = false;
             }
-         }
+        }
     }
+
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<vector<char>> board(n,vector<char>(n,'.'));
-        int col = 0;
-        solve(ans,board,col,n);
+        vector<vector<string>> ans;
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+
+        solve(n, ans, board, 0);
         return ans;
     }
 };
