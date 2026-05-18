d70cd3d49dae3cea50bafa48df0041e80c948215
Aditya Sharma <adityasharma736630@gmail.com>
2026-05-18 22:13:26 +0530

    Time: 7 ms (54.12%) | Memory: 43.8 MB (93.90%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
@@ -10,32 +10,65 @@
  */
 class Solution {
 public:
-    void nextLarge(vector<int>& temp , vector<int>& ans){
-        stack<int>st;
-        st.push(-1);
-        int n = temp.size();
-        for(int i = n-1;i>=0;i--){
-            while(st.top() != -1 && temp[i]>=st.top()){
-                st.pop();
-            }
-            st.top() == -1 ? ans.push_back(0) : ans.push_back(st.top());
-            st.push(temp[i]);
-        }
-        reverse(ans.begin(),ans.end());
+    // void nextLarge(vector<int>& temp , vector<int>& ans){
+    //     stack<int>st;
+    //     st.push(-1);
+    //     int n = temp.size();
+    //     for(int i = n-1;i>=0;i--){
+    //         while(st.top() != -1 && temp[i]>=st.top()){
+    //             st.pop();
+    //         }
+    //         st.top() == -1 ? ans.push_back(0) : ans.push_back(st.top());
+    //         st.push(temp[i]);
+    //     }
+    //     reverse(ans.begin(),ans.end());
+    // }
+    // vector<int>nextLargerNode(ListNode* head){
+    //     vector<int>temp;
+    //     while(head){
+    //         int val = head->val;
+    //         temp.push_back(val);
+    //         head = head->next;
+    //     }
+    //     vector<int>ans;
+    //     nextLarge(temp,ans);
+
+    //     return ans;
+    // }
+    ListNode* reverse(ListNode* head,ListNode* prev){
+        if(!head) return prev;
+
+        ListNode* tempHead = head->next;
+        head->next = prev;
+        return reverse(tempHead,head);
     }
-    vector<int>nextLargerNode(ListNode* head){
-        vector<int>temp;
-        while(head){
-            int val = head->val;
-            temp.push_back(val);
-            head = head->next;
-        }
+    vector<int>littleOptimse(ListNode* head){
+
         vector<int>ans;
-        nextLarge(temp,ans);
+        stack<ListNode*> st;
+
+        ListNode* prev=  reverse(head,nullptr);
+
+        while(prev){
+            while(!st.empty() && st.top()->val <= prev->val){
+
+                st.pop();
+
+            }
+            st.empty() ? ans.push_back(0) : ans.push_back(st.top()->val);
+
+            st.push(prev);
+            prev = prev->next;
+        }
+
+        std::reverse(ans.begin(),ans.end());
 
         return ans;
+        
+
     }
     vector<int> nextLargerNodes(ListNode* head) {
-        return nextLargerNode(head);
+        // return nextLargerNode(head);
+        return littleOptimse(head);
     }
 };
