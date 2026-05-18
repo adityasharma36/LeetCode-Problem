a8f61d436c475427dba7b95bb4d32bd99bf1ef2e
Aditya Sharma <adityasharma736630@gmail.com>
2026-05-18 21:42:15 +0530

    Time: 3 ms (81.35%) | Memory: 14.3 MB (33.04%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    string removeDuplicates(string s) {
-        stack<char> st;
+    string stackMethod(string s){
+           stack<char> st;
         for(auto i : s){
             if(st.empty()){
                 st.push(i);
@@ -12,16 +12,29 @@ public:
                 st.push(i);
             }
         }
-        
-        // Pop elements out of the stack into a string
+
         string ans = "";
         while (!st.empty()) {
             ans += st.top();
             st.pop();
         }
-        
-        // Since stack is LIFO, the string is reversed. Reverse it back.
         reverse(ans.begin(), ans.end());
         return ans;
+    }
+    string optimseMethod(string s){
+        string ans = "";
+        for(auto i : s){
+            if(!ans.empty() && ans.back()== i){
+                ans.pop_back();
+            }else{
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+    string removeDuplicates(string s) {
+     
+        // return stackMethod(s);
+        return optimseMethod(s);
     }
 };
