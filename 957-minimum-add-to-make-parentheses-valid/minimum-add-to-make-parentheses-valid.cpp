510239487a3d4d350d42d4802e4f15b4c865ceb6
Aditya Sharma <adityasharma736630@gmail.com>
2026-05-20 23:27:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.36%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    int addToMake(string s){
+    int bruteForce(string s){
         stack<char>st;
-        for(int i = 0;i<s.size();i++){
-            if(s[i]=='('){
-                st.push('(');
+
+        for(auto i: s){
+            if(i=='('){
+                st.push(i);
             }else{
                 if(!st.empty() && st.top()=='('){
                     st.pop();
                 }else{
-                    st.push(s[i]);
+                    st.push(i);
                 }
             }
         }
         return st.size();
     }
-    int makeToAdd(string s){
-        int open = 0;
-        int need = 0;
-        for(auto i: s){
-            if(i == '('){
-                open++;
-            }else{
-                if(open>0){
-                    open--;
-                }else{
-                    need++;
-                }
-            }
-        }
-        return open+need;
-    }
     int minAddToMakeValid(string s) {
-    //  return addToMake(s);
-        return makeToAdd(s);
+        return bruteForce(s);
     }
 };
