1948af2369cd744b66a07f708d8c6ab49eee7a4c
Aditya Sharma <adityasharma736630@gmail.com>
2026-05-25 15:49:34 +0530

    Time: 17 ms (80.12%) | Memory: 139 MB (93.98%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    vector<int> slidingWindow(vector<int>& nums, int k) {
-        deque<int> q;
-        vector<int> ans;
-        
-        // Step 1: Process first window
-        for(int i = 0; i < k; i++) {
-            while(!q.empty() && nums[q.back()] <= nums[i])
-                q.pop_back();
-            q.push_back(i);
+    vector<int>slidingWindow(vector<int>& nums,int k){
+        vector<int>ans;
+        deque<int>dq;
+        for(int i = 0;i<nums.size();i++){
+            if(!dq.empty() && i-k == dq.front()) dq.pop_front();
+
+            while(!dq.empty() && nums[dq.back()] <= nums[i]){ dq.pop_back();};
+
+            dq.push_back(i);
+            if(i>=k-1){
+                ans.push_back(nums[dq.front()]);
+            }
         }
-        ans.push_back(nums[q.front()]);
-        
-        // Step 2: Process rest of the windows
-        for(int i = k; i < nums.size(); i++) {
-            // remove elements out of this window
-            if(!q.empty() && q.front() <= i - k)
-                q.pop_front();
-            
-            // remove smaller elements
-            while(!q.empty() && nums[q.back()] <= nums[i])
-                q.pop_back();
-            
-            q.push_back(i);
-            ans.push_back(nums[q.front()]);
-        }
-        
         return ans;
     }
-
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        return slidingWindow(nums, k);
+        return slidingWindow(nums,k);
     }
 };
