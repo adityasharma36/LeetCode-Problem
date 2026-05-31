1b3de580127da86beacb011dd0a72f65a3ad1ea3
Aditya Sharma <adityasharma736630@gmail.com>
2026-05-31 18:43:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (14.31%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -19,8 +19,29 @@ public:
         }
         return newString;
     }
+    string removeParent(string s){
+        stack<char>st;
+        string newparen;
+        for(auto i: s){
+            if(i == '('){
+                if( !st.empty() && st.top()== '('){
+                    newparen.push_back(i);
+                }
+                st.push(i);
+            }else{
+                if(st.size()>1){
+                    newparen.push_back(i);
+                    
+                }
+                st.pop();
+            }
+        }
+        return newparen;
+
+    }
     string removeOuterParentheses(string s) {
-        string removeOuter = removerParenthese(s);
-        return removeOuter;
+        // string removeOuter = removerParenthese(s);
+        string stackMethod = removeParent(s);
+        return stackMethod;
     }
 };
