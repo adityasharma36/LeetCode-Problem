512ebbb1299bfe06ed692a4c662088e9c35dcf09
Aditya Sharma <adityasharma736630@gmail.com>
2026-05-31 16:33:57 +0530

    Time: 30 ms (9.06%) | Memory: 21.4 MB (14.03%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,64 +1,39 @@
 class Solution {
 public:
-    void setRowZero(vector<vector<int>>& nums,int row){
-        for(int i = 0;i<nums.size();i++){
-            if(nums[row][i]!=0){
-                nums[row][i]=-1;
-            }
+    void setCol(vector<vector<int>>&temp,int col){
+        for(int i =0;i<temp.size();i++){
+            temp[i][col]= 0;
         }
     }
-    void setColZero(vector<vector<int>>& nums,int col){
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i][col]!=0){
-                nums[i][col]=-1;
-            }
+    void setRow(vector<vector<int>>&temp,int row){
+        int n = temp[0].size();
+        for(int i =0;i<n;i++){
+            temp[row][i]= 0;
         }
     }
-    void zeroesSet(vector<vector<int>>& nums){
-
-        for(int i = 0;i<nums.size();i++){
-            for(int j = 0;j<nums.size();j++){
-                if(nums[i][j]==0){
-                    setRowZero(nums,i);
-                    setColZero(nums,j);
+    void bruteForce(vector<vector<int>>& matrix){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>>temp(m,vector<int>(n,1));
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(matrix[i][j]==0){
+                    setCol(temp,j);
+                    setRow(temp,i);
                 }
             }
         }
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
 
-        for(int i = 0;i<nums.size();i++){
-            for(int j = 0;j<nums.size();j++){
-                if(nums[i][j]==-1){
-                    nums[i][j]=0;
-                }
-            }
-        }
-    }
-    void betterSol(vector<vector<int>>& nums){
-        int row = nums.size();
-        int col = nums[0].size();
+                if(temp[i][j]==0){
 
-        vector<int> rowArr(row,0);
-        vector<int>colArr(col,0);
-
-        for(int i = 0;i<row;i++){
-            for(int j = 0;j<col;j++){
-                if(nums[i][j]==0){
-                    rowArr[i]=1;
-                    colArr[j]=1;
-                }
-            }
-        }
-
-        for(int i = 0;i<row;i++){
-            for(int j = 0;j<col;j++){
-                if(rowArr[i]==1 || colArr[j]==1){
-                    nums[i][j]=0;
+                matrix[i][j]= 0;
                 }
             }
         }
     }
     void setZeroes(vector<vector<int>>& matrix) {
-        // zeroesSet(matrix);
-        betterSol(matrix);
+        bruteForce(matrix);
     }
 };
