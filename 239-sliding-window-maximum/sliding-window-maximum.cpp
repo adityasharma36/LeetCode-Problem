eb871a2e6866b351d18edf08474de55b558bd032
Aditya Sharma <adityasharma736630@gmail.com>
2026-06-03 23:27:59 +0530

    Time: 20 ms (69.12%) | Memory: 139 MB (89.03%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,25 +1,87 @@
 class Solution {
 public:
-    vector<int>maxSliding(vector<int>& nums,int k){
+    vector<int> slidingWindow(vector<int>&nums,int k){
         deque<int>dq;
         vector<int>ans;
-        for(int i = 0;i<nums.size();i++){
-            if(!dq.empty() && i-k == dq.front()){
-                dq.pop_front();
-            }
+        int n = nums.size();
 
-            while(!dq.empty() && nums[dq.back()]<=nums[i]){
-                dq.pop_back();
+        for(int i = 0;i<k;i++){
+            if(!dq.empty()){
+                while(!dq.empty() && nums[dq.back()] <= nums[i]){
+                    dq.pop_back();
+                }
+                cout<<dq.front()<<endl;
             }
             dq.push_back(i);
 
-            if(i>=k-1){
-                ans.push_back(nums[dq.front()]);
+        }
+    
+
+        for(int i = k;i<n;i++){
+
+            // additon of element 
+            if(!dq.empty()){
+
+                int index = dq.front();
+
+                int ele = nums[index];
+
+                ans.push_back(ele);
+
+                cout<<"here is properly working 0"<<endl;
+
             }
+
+            // removal of element 
+
+            if(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
+                cout<<"here is properly workind 1"<<endl;
+            }
+
+            // add new present element in queue
+
+            while(!dq.empty() && nums[dq.back()]<=nums[i]){
+                dq.pop_back();
+                cout<<"here properly working 2"<<endl;
+            }
+            dq.push_back(i);
+        }
+        if(!dq.empty()){
+            ans.push_back(nums[dq.front()]);
+            cout<<"here is properly 3"<<endl;
+        }
+
+        return ans;
+    }
+    vector<int>littleOptimse(vector<int>& nums,int k){
+        deque<int>dq;
+        vector<int>ans;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+
+            // removal of new Element 
+            if(!dq.empty() && dq.front()==i-k){
+                dq.pop_front();
+            }
+
+            // addition of new Element
+
+            while(!dq.empty() && nums[dq.back()] <= nums[i]){
+                dq.pop_back();          
+            }
+        
+            dq.push_back(i);
+
+        // addition of element in ans;
+        if(i>=k-1){
+            ans.push_back(nums[dq.front()]);
+        }
         }
         return ans;
     }
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        return maxSliding(nums,k);
+        // return slidingWindow(nums,k);
+        return littleOptimse(nums,k);
     }
 };
