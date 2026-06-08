e885ffafa666ea175946e5fd8cfb706243216c5d
Aditya Sharma <adityasharma736630@gmail.com>
2026-06-08 18:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (14.53%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int maxSubArrayFnc(vector<int>& nums){
-        int maximumSum = INT_MIN;
+    int kadaneAlgo(vector<int>& nums){
+        int maxSub = INT_MIN;
         int tempSum = 0;
-        for(int i = 0;i<nums.size();i++){
-            tempSum+=nums[i];
 
-            if(tempSum> maximumSum){
-                maximumSum = tempSum;
-            }
+        for(auto i: nums){
+            tempSum+=i;
+            maxSub = max(maxSub,tempSum);
             if(tempSum<0){
-                tempSum = 0;
+                tempSum =0;
             }
         }
-        return maximumSum;
+        return maxSub;
     }
     int maxSubArray(vector<int>& nums) {
-        return maxSubArrayFnc(nums);
+        return kadaneAlgo(nums);
     }
 };
