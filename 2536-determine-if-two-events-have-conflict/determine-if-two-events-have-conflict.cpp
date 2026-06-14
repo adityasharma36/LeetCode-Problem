31c173cc526cfe24454d0e19c3ffe1ae913fe693
Aditya Sharma <adityasharma736630@gmail.com>
2026-06-14 10:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (64.65%) - LeetSync

--- 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.cpp
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-            int eve1St = stoi(event1[0].substr(0,2))* 60 + stoi(event1[0].substr(3,5));
-            int eve1En = stoi(event1[1].substr(0,2))* 60 + stoi(event1[1].substr(3,5));
-            int eve2St = stoi(event2[0].substr(0,2))* 60 + stoi(event2[0].substr(3,5));
-            int eve2En = stoi(event2[1].substr(0,2))* 60 + stoi(event2[1].substr(3,5));
+            // int eve1St = stoi(event1[0].substr(0,2))* 60 + stoi(event1[0].substr(3,5));
+            // int eve1En = stoi(event1[1].substr(0,2))* 60 + stoi(event1[1].substr(3,5));
+            // int eve2St = stoi(event2[0].substr(0,2))* 60 + stoi(event2[0].substr(3,5));
+            // int eve2En = stoi(event2[1].substr(0,2))* 60 + stoi(event2[1].substr(3,5));
 
-            vector<pair<int,int>>even;
-            even.push_back({eve1St,eve1En});
-            even.push_back({eve2St,eve2En});
+            // vector<pair<int,int>>even;
+            // even.push_back({eve1St,eve1En});
+            // even.push_back({eve2St,eve2En});
 
-            sort(even.begin(),even.end());
+            // sort(even.begin(),even.end());
 
 
-            auto i = even[0];
+            // auto i = even[0];
 
-            auto j = even[1];
+            // auto j = even[1];
 
-            if(j.first<=i.second) return true;
-            return false;
+            // if(j.first<=i.second) return true;
+            // return false;
+
+            return event1[0] <= event2[1] && event2[0] <= event1[1];
 
 
     }
