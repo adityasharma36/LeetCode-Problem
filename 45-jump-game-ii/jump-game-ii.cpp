84dbda2e2591e055a23c7c342bfc5e73d4c3b21f
Aditya Sharma <adityasharma736630@gmail.com>
2026-06-18 17:37:28 +0530

    Time: 71 ms (17.66%) | Memory: 21.8 MB (14.13%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -32,10 +32,34 @@ public:
 
         return dp[i] = min_jump;
     }
+    int topBottom(vector<int>& nums){
+        int n = nums.size();
+        vector<int>dp(n+1,0);
+
+        for(int i = n-2;i>=0;i--){
+            int min_jump = 1e9;
+            for(int j = 1;j<=nums[i];j++){
+                if(j+i<=n){
+                    int jumps = dp[j+i];
+
+                    if(jumps != 1e9){
+
+                        min_jump = min(min_jump,1+ jumps);
+
+                    }
+                }
+            }  
+            dp[i] = min_jump;
+
+        }
+        return dp[0];
+    }
     int jump(vector<int>& nums) {
         // return minJump(nums,0);
-        int n = nums.size();
-        vector<int>dp(n,-1);
-        return memo(nums,0,dp);
+        // int n = nums.size();
+        // vector<int>dp(n,-1);
+        // return memo(nums,0,dp);
+
+        return topBottom(nums);
     }
 };
