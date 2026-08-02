33937f8fad15f1e7466115a163a857b9b52a2d4b
Aditya Sharma <adityasharma736630@gmail.com>
2026-08-02 21:38:20 +0530

    Time: 2 ms (84.85%) | Memory: 11.6 MB (36.89%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    vector<int>anagram(string s,string p){
-        vector<int> ans;
-        if(s.size()<p.size()) return ans;
-
-        int start = 0;
-        int right = 0;
-        int count = p.size();
-        unordered_map<char,int>um;
-        for(auto i: p){
-            um[i]++;
+    bool isZero(vector<int>&count){
+        for(auto &i: count){
+            if(i!=0) return false;
         }
-        for(int i = 0;i<s.size();i++){
-            
-            char ch = s[i];
-            if(um.find(ch)!= um.end()){
-                if(um[ch]>0)count--;
-                um[ch]--;
-            }
-
-            if(i-start+1 == p.size()){
-                if(count == 0){
-                    ans.push_back(start);
-                }
-
-                char leftChar = s[start];
-                if(um.find(leftChar)!= um.end()){
-                    if(um[leftChar]>=0)count++;
-                    um[leftChar]++;
-                }
-
-                start++;
-
-            }
-        }
-        return ans;
+        return true;
     }
     vector<int> findAnagrams(string s, string p) {
-        return anagram(s,p);
+        vector<int>ans;
+        int n = s.size();
+        int m = p.size();
+
+        vector<int>count(26,0);
+        for(auto i:  p){
+            count[i-'a']++;
+        }
+
+        int index = 0;
+        int j = 0;
+        while(j<n){
+            count[s[j]-'a']--;
+            if(j-index + 1 == m){
+                if(isZero(count)){
+                    ans.push_back(index);
+                }
+                count[s[index]-'a']++;
+                index++;
+            }
+            j++;
+        }
+        return ans;
     }
 };
