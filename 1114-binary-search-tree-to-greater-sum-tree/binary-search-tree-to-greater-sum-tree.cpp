e756a3904c8496133068565ff499e188f230ca3a
Aditya Sharma <adityasharma736630@gmail.com>
2026-08-05 21:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (51.49%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+     public:
+      int sum = 0; 
+      void solve(TreeNode* root)
+       { 
+        if (!root) return; 
+        solve(root->right); 
+        sum += root->val; 
+        root->val = sum; 
+        solve(root->left); 
+        } 
+        TreeNode* bstToGst(TreeNode* root) 
+        { 
+            solve(root); return root;
+         }
+         };
