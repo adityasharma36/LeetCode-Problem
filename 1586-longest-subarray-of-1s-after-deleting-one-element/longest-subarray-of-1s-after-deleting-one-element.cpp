c7a73d142518f1db6c363c95fcb8044d7a005108
Aditya Sharma <adityasharma736630@gmail.com>
2026-08-10 21:09:39 +0530

    Time: 1 ms (32.86%) | Memory: 60.1 MB (49.85%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums) {
-        int i = 0;
+    int twoSliding(vector<int>& nums){
+         int i = 0;
         int j = 0;
         int size = nums.size();
         int ans = 0;
@@ -20,7 +20,29 @@ public:
             j++;
         }
      
-        if(countZero ==0) return ans;
+      
         return ans;
+    }
+    int oneSliding(vector<int>& nums){
+        int i = 0;
+        int j = 0;
+        int size = nums.size();
+        int oneIndex = -1;
+        int ans = 0;
+        while(j<size){
+            if(nums[j]==0 && oneIndex != -1){
+                i = oneIndex+1;
+            }
+            if(nums[j]==0){
+                oneIndex = j;
+            }
+            ans = max(ans,j-i);
+            j++;
+        }
+        return ans;
+        }
+    int longestSubarray(vector<int>& nums) {
+    //    return twoSliding(nums);
+        return oneSliding(nums);
     }
 };
