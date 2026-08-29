20893587d55b6f60ee84460aada7f0714b95e15c
Aditya Sharma <adityasharma736630@gmail.com>
2026-08-29 11:19:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (24.06%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    int robber(vector<int>& nums,int index){
-        if(index>=nums.size()) return 0;
+    int recursiveSol(vector<int>&nums,int i){
+        if(i>=nums.size()) return 0;
 
-        int inclde = nums[index] + robber(nums,index+2);
-        int exlucde = 0 + robber(nums,index+1);
-        return max(inclde,exlucde);
+        int incl = nums[i] + recursiveSol(nums,i+2);
+        int excl = 0 + recursiveSol(nums,i+1);
+
+        return max(incl,excl);
     }
-    int memo(vector<int>& nums, int index, vector<int>& dp) {
-        if(index >= nums.size()) return 0;
+    int solveByMemo(vector<int>& nums,int i,vector<int>&dp){
+        if(i>=nums.size()) return 0;
+        if(dp[i] != -1) return dp[i];
 
-        if(dp[index] != -1) return dp[index];
+        int incl = nums[i] + solveByMemo(nums,i+2,dp);
+        int excl = solveByMemo(nums,i+1,dp);
 
-        int include = nums[index] + memo(nums, index + 2, dp);
-        int exclude = memo(nums, index + 1, dp);
-
-        dp[index] = max(include, exclude);
-        return dp[index];
-    }
-    int tabu(vector<int>& nums){
-
-        vector<int>dp(nums.size()+2,0);
-
-        for(int i = nums.size()-1;i>=0;i--){
-
-        int include = nums[i] + dp[i + 2];
-
-        int exclude = dp[i + 1];
-
-        dp[i] = max(include, exclude);
-
-        }
-        return dp[0];
+        dp[i] = max(incl,excl);
+        return dp[i];
     }
     int rob(vector<int>& nums) {
-        // vector<int> dp(nums.size(), -1);
-        // return memo(nums, 0, dp);
-        return tabu(nums);
+        // return recursiveSol(nums,0);
+        vector<int>dp(nums.size()+1,-1);
+        return solveByMemo(nums,0,dp);
+
     }
 };
