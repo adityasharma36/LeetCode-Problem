823392af8a2e4ae8f7d69a671c355bc73c5f2666
Aditya Sharma <adityasharma736630@gmail.com>
2026-08-29 13:08:48 +0530

    Time: 659 ms (5.07%) | Memory: 296.6 MB (6.94%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,55 +1,33 @@
 class Solution {
 public:
-    // int recursiveSol(vector<int>& nums,int lastIndex,int curr){
-    //     if(curr>=nums.size()) return 0;
-
-    //     int curEle = nums[curr];
-    //     int inc = 0;
-    //     if(lastIndex == -1 || curEle > nums[lastIndex]){
-    //         inc = 1 + recursiveSol(nums,curr,curr+1);
-    //     }
-    //     int exc = recursiveSol(nums,lastIndex , curr+1);
-
-    //     int finalAns = max(inc,exc);
-    //     return finalAns;
-    // }
-    int solveByMemo(vector<int>& nums,vector<vector<int>>& dp,int lastIndex,int curr){
-        if(curr>= nums.size()) return 0;
-
-        if(dp[lastIndex+1][curr]!= -1) return dp[lastIndex+1][curr];
-
-        int incld = 0;
-        int curEle = nums[curr];
-        if(lastIndex==-1 || curEle > nums[lastIndex]){
-            incld = 1+ solveByMemo(nums,dp,curr,curr+1);
+    int solveByReco(vector<int>& nums,int lastIndex,int curr){
+        if(curr>=nums.size()) return 0;
+        int incl = 0;
+        if(lastIndex == -1 || nums[curr]>nums[lastIndex]){
+            incl = 1+ solveByReco(nums,curr,curr+1);
         }
-        int excld = solveByMemo(nums,dp,lastIndex,curr+1);
+        int excl = 0 + solveByReco(nums,lastIndex,curr+1);
 
-        dp[lastIndex+1][curr]= max(incld,excld);
-        return dp[lastIndex+1][curr];
+        return max(incl,excl);
     }
-    int solveByTab(vector<int>& nums){
-         vector<vector<int>>dp(nums.size()+2,vector<int>(nums.size()+2,0));
+    int solveByMemo(vector<int>& nums,int lastIndex,int curr,vector<vector<int>>& dp){
+        if(curr>=nums.size()) return 0;
+        if(dp[curr][lastIndex+1] != -1) return dp[curr][lastIndex+1];
+        int incl = 0;
+        if(lastIndex == -1 || nums[curr] > nums[lastIndex]){
+            incl = 1+ solveByMemo(nums,curr,curr+1,dp);
+        }
+        int excl = solveByMemo(nums,lastIndex,curr+1,dp);
 
-         for(int i = nums.size()-1;i>=-1;i--){
-            for(int j = nums.size()-1;j>=0;j--){
-                   int incld = 0;
+        dp[curr][lastIndex+1] = max(incl,excl);
 
-            int curEle = nums[j];
-            if(i==-1 || curEle > nums[i]){
-            incld = 1+ dp[j+1][j+1];
-            }
-            int excld = dp[i + 1][j + 1];
-
-            dp[i+1][j]= max(incld,excld);
-            }
-         }
-         return dp[0][0];
+        return dp[curr][lastIndex+1];
     }
     int lengthOfLIS(vector<int>& nums) {
-        // return recursiveSol(nums,-1,0);
-        // vector<vector<int>>dp(nums.size()+2,vector<int>(nums.size()+2,-1));
-        // return solveByMemo(nums,dp,-1,0);
-        return solveByTab(nums);
+        // return solveByReco(nums,-1,0);
+        int n = nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+2,-1));
+        return solveByMemo(nums,-1,0,dp);
+
     }
 };
