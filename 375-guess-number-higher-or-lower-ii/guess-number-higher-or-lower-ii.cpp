1dc4984e93bc855d1fd330918a9392cd27a4779b
Aditya Sharma <adityasharma736630@gmail.com>
2026-09-06 22:09:41 +0530

    Time: 31 ms (93.39%) | Memory: 12.8 MB (5.44%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -18,10 +18,28 @@ public:
         dp[s][e] = ans;
         return dp[s][e];
     }
+    int solveByTabu(int n){
+        vector<vector<long long>>dp(n+1,vector<long long>(n+1,0));
+
+        for(int s = n;s>=1;s--){
+            for(int e = 1;e<=n;e++){
+                if(s >=e){
+                    continue;
+                }
+                long long ans = INT_MAX;
+                for(int i = s;i<e;i++){
+                        ans = min(ans,i+ max(dp[s][i-1] , dp[i+1][e]));
+                    }
+                dp[s][e] = ans;
+            }
+        }
+        return dp[1][n];
+    }
     int getMoneyAmount(int n) {
         // int ans = solveByRec(1,n);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans = solveByMemo(1,n,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // int ans = solveByMemo(1,n,dp); 
+        int ans = solveByTabu(n);
         return ans;
     }
 };
