806ae9f4c83ba27243a77f7d420551c363facbc2
Aditya Sharma <adityasharma736630@gmail.com>
2026-09-18 18:58:40 +0530

    Time: 1 ms (35.23%) | Memory: 13.5 MB (42.56%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -46,11 +46,36 @@ public:
 
         return dp[i] = min({day1,day7,day30});
     }
+    int solveByTabu(vector<int>& days,vector<int>& costs){
+        int n = days.size();
+        vector<int>dp(n+1,0);
+
+        for(int i = n-1;i>=0;i--){
+            int day1 = costs[0] + dp[i+1];
+
+            int presentDays = 7 + days[i] -1;
+            int j = i;
+            while(j<days.size() && days[j] <= presentDays){
+                j++;
+            }
+            int day7 = costs[1] + dp[j];
+            j = i;
+            presentDays = days[i] + 30 -1;
+            while(j<days.size() && days[j] <= presentDays){
+                j++;
+            }
+            int day30 = costs[2] + dp[j];
+
+             dp[i] = min({day1,day7,day30});
+        }
+        return dp[0];
+    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         // int ans = solveByRec(days,costs,0);
-        int n = days.size();
-        vector<int>dp(n+1,-1);
-        int ans = solveByMemo(days,costs,dp,0);
+        // int n = days.size();
+        // vector<int>dp(n+1,-1);
+        // int ans = solveByMemo(days,costs,dp,0);
+        int ans = solveByTabu(days,costs);
         return ans;
 
     }
