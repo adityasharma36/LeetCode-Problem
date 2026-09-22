4cd0dffd592b76d1722a2bf8ac8afad1171438aa
Aditya Sharma <adityasharma736630@gmail.com>
2026-09-22 23:36:01 +0530

    Time: 17 ms (63.50%) | Memory: 146.4 MB (15.38%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -23,11 +23,27 @@ public:
         }
         return dp[i]= ans;
     }
+    int solveByTabu(vector<int>& stone){
+        int n = stone.size();
+        vector<int>dp(n+1,0);
+        for(int i = n-1;i>=0;i--){
+            int ans = INT_MIN;
+            int sum = 0;
+            for(int x = 1;x<=3;x++){
+                if(x+i-1 >= stone.size()) break;
+                sum+=stone[x+i-1];
+                ans = max(ans,sum- dp[i+x]);
+            }
+            dp[i]= ans;
+        }
+        return dp[0];
+    }
     string stoneGameIII(vector<int>& stoneValue) {
         // int ans = solveByRec(stoneValue,0);
-        int n = stoneValue.size();
-        vector<int>dp(n+1,-1);
-        int ans = solveByMemo(stoneValue,0,dp);
+        // int n = stoneValue.size();
+        // vector<int>dp(n+1,-1);
+        // int ans = solveByMemo(stoneValue,0,dp);
+        int ans = solveByTabu(stoneValue);
         if(ans >0) return "Alice";
         if(ans<0) return "Bob";
         return "Tie";
